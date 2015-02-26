ab4a41b040d6626072ee2e50b0c8709c95d181d7
Mark Wagner <mark.wagner@reachlocal.com>
2015-02-26 10:53:15 -0600

    Working with Rest with groovy

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -1,5 +1,9 @@
-
-
+@Grapes(
+        @Grab(group = 'org.codehaus.groovy.modules.http-builder',
+                module = 'http-builder',
+                version = '0.6')
+)
+import groovyx.net.http.RESTClient
 
 def file = new File('../data/fells_loop.gpx')
 
@@ -17,10 +21,20 @@ gpx.with {
     println attributes()['creator']
 }
 
+def forecastApi = new RESTClient('https://api.forecast.io/')
+def apiKey ='f106310bab9d2b16dfaa442bb6b380e7'
+
 gpx.rte.rtept.each({
     println it.@lat
     println it.@lon
 
     def parser = new DateParser()
     println parser.parse(it.time.toString())
+
+    def queryString = "forecast/$apiKey/${it.@lat},${it.@lon},${it.time}"
+    def response = forecastApi.get(path: queryString)
+
+    println response.data.currently.summary
+    println response.data.currently.temperature + " degrees"
+
 })
